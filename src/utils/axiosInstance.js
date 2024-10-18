73562ab4b71e98705d6499ff395885fcf414e2a8
Shashidhar Sn <snshashidhar3@gmail.com>
2024-10-18 13:24:41 -0700

    url change

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api";
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const axiosInstance = axios.create({
   baseURL: backendUrl,
